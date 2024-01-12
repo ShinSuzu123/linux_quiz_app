3b9966fc3acde3d68ac415d30a84a8c7138dd558
ShinSuzu123 <shnn-suzuki@usen-next.jp>
2024-01-12 13:23:06 +0900

    振り出しに戻す

--- l_101/get_questions.php
+++ l_101/get_questions.php
@@ -4,18 +4,12 @@ $username = "testuser";
 $password = "testpass";
 $dbname = "lpic_quiz";
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password); 
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     // 特定のカテゴリーの問題一覧を取得
     $category = isset($_GET['category']) ? $_GET['category'] : '';
-    // var_dump($_GET);
     $stmt = $conn->prepare("SELECT * FROM questions WHERE category = :category");
     $stmt->bindParam('category', $category);
     $stmt->execute();
