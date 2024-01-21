520f2de61e746a9c755d01f1e1c9b1081076acea
ShinSuzu123 <shnn-suzuki@usen-next.jp>
2024-01-22 00:07:14 +0900

    コメント追加した

--- certification/logout.php
+++ certification/logout.php
@@ -1,16 +1,16 @@
 <?php
 session_start(); // セッションを開始する
 
-// セッションクッキーを削除する
-if(ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
+// 使用しているセッションクッキーを削除する
+if(ini_get("session.use_cookies")) { // セッションがクッキーをクッキーを使用しているかどうかの確認 
+    $params = session_get_cookie_params(); // セッションクッキーのパラメーターを取得する
+    setcookie(session_name(), '', time() - 42000, // クッキーの有効期限を現在時刻より過去にすることでブラウザはそのクッキーを削除する
         $params["path"], $params["domain"],
         $params["secure"], $params["httponly"]
-    );
+    ); // setcookie関数を使用してセッションクッキーを削除する。
 }
 
-// セッション破棄
+// 全てのセッション破棄
 session_destroy();
 
 // ログアウト後のリダイレクト先
