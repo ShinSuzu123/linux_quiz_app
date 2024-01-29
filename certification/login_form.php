9e4d0945f1b66b00c17f5e49b539f48e9745500a
ShinSuzu123 <shnn-suzuki@usen-next.jp>
2024-01-29 11:27:00 +0900

    ログインページ改修

--- certification/login_form.php
+++ certification/login_form.php
@@ -16,30 +16,35 @@ if ($conn->connect_error) {
     die("データベースへの接続失敗しました:" . $conn->connect_error); // 接続エラーがあればエラーメッセージを出力して処理を中断
 }
 
-// フォームからのデータを受け取る
-$email = $_POST['email'];
-$password = $_POST['password'];
+// POSTメソッドで送信されたかどうかの確認
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // フォームからのデータを受け取る
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-// SQLのクエリの作成と実行
-// ユーザーのメールアドレスに基づいてデータベースからユーザー情報を取得
-$stmt = $conn->prepare("SELECT id, email, password FROM users WHERE email = ?");
-$stmt->bind_param("s", $email);
-$stmt->execute();
-$result = $stmt->get_result();
+    // SQLのクエリの作成と実行
+    // ユーザーのメールアドレスに基づいてデータベースからユーザー情報を取得
+    $stmt = $conn->prepare("SELECT id, email, password FROM users WHERE email = ?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-// パスワード検証とリダイレクト
-if ($row = $result->fetch_assoc()) {
-    // パスワードの検証
-    if (password_verify($password, $row['password'])) {
-        // パスワードが一致する場合
-        header("Location: ../101_quiz.html");
+    // パスワード検証とリダイレクト
+    if ($row = $result->fetch_assoc()) {
+        // パスワードの検証
+        if (password_verify($password, $row['password'])) {
+            $_SESSION['user_id'] = $row['id'];
+            // パスワードが一致する場合
+            echo "<script>alert('ログイン成功'); window.location.href='../101_quiz.html'</script>";
+            // header("Location: ../101_quiz.html");
+        } else {
+            // パスワードが一致しない場合
+            echo "<script>alert('ログインに失敗しました'); window.location.href='login.html'</script>";
+        }
     } else {
-        // パスワードが一致しない場合
-        echo "ログインに失敗しました";
+        // メールアドレスが存在しない場合
+        echo "<script>alert('ログインに失敗しました'); window.location.href='login.html'</script>";
     }
-} else {
-    // メールアドレスが存在しない場合
-    echo "ログインに失敗しました";
 }
 
 // DB接続のクローズ
