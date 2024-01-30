5ce42cc70a3906c9be624c0daecaf9f7b105eecf
ShinSuzu123 <shnn-suzuki@usen-next.jp>
2024-01-30 18:28:24 +0900

    管理者ログインの制限

--- admin/admin_login_form.php
+++ admin/admin_login_form.php
@@ -20,27 +20,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    // SQLのクエリの作成と実行
-    // ユーザーのメールアドレスに基づいてデータベースからユーザー情報を取得
-    $stmt = $conn->prepare("SELECT id, email, password FROM users WHERE email = ?");
-    $stmt->bind_param("s", $email);
-    $stmt->execute();
-    $result = $stmt->get_result();
+    if ($email == "shnn-suzuki@usen-next.jp") {
+        // SQLのクエリの作成と実行
+        // ユーザーのメールアドレスに基づいてデータベースからユーザー情報を取得
+        $stmt = $conn->prepare("SELECT id, email, password FROM users WHERE email = ?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-    // パスワード検証とリダイレクト
-    if ($row = $result->fetch_assoc()) {
-        // パスワードの検証
-        if (password_verify($password, $row['password'])) {
-            $_SESSION['user_id'] = $row['id'];
-            // パスワードが一致する場合
-            echo "<script>alert('ログイン成功'); window.location.href='admin_quiz.php'</script>";
+        // パスワード検証とリダイレクト
+        if ($row = $result->fetch_assoc()) {
+            // パスワードの検証
+            if (password_verify($password, $row['password'])) {
+                $_SESSION['user_id'] = $row['id'];
+                // パスワードが一致する場合
+                echo "<script>alert('ログイン成功'); window.location.href='admin_quiz.php'</script>";
+            } else {
+                // パスワードが一致しない場合
+                echo "<script>alert('ログインに失敗しました'); window.location.href='admin_login.html'</script>";
+            }
         } else {
-            // パスワードが一致しない場合
-            echo "<script>alert('ログインに失敗しました'); window.location.href='admin_login.html'</script>";
+            // メールアドレスが存在しない場合
+            echo "ログインに失敗しました";
         }
     } else {
-        // メールアドレスが存在しない場合
-        echo "ログインに失敗しました";
+        echo "<script>alert('アクセス権限がありません'); window.location.href='admin_login.html'</script>";
     }
 }
 
